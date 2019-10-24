f2395b468550c73686cae746bbe74da05fce68d1
Abraham Vazquez <abxvazquez23@gmail.com>
2019-10-24 17:30:35 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	align_behavior-3.py

--- align_behavior_cell_traces.py
+++ align_behavior_cell_traces.py
@@ -2,7 +2,6 @@ import csv
 import pandas as pd
 from numpy import *
 from operantanalysis import *
-from operator import itemgetter
 from tkinter import filedialog, Tk
 
 
@@ -17,21 +16,21 @@ def choose_file():
 
 def parse_csv():
     csv_file = open(choose_file())
-    my_csv = csv.reader(csv_file)
-    parsed_csv = list(my_csv)
-    header = parsed_csv[0]
-    for index1, row in enumerate(parsed_csv):
-        for index2, item in enumerate(row):
-            try:
-                parsed_csv[index1][index2] = round((float(item)), 2)
-            except ValueError:
-                pass
-    csv_list = sorted(parsed_csv[1:], key=itemgetter(0))
-    csv_list.insert(0, header)
-    with open('Ordered_EDs.csv', 'w', newline='') as csvFile:
-        writer = csv.writer(csvFile, delimiter=',')
-        writer.writerows(csv_list)
-    return csv_list
+    #my_csv = csv.reader(csv_file)
+    #parsed_csv = list(my_csv)
+    #header = parsed_csv[0]
+   # for index1, row in enumerate(parsed_csv):
+     #   for index2, item in enumerate(row):
+      #      try:
+      #          parsed_csv[index1][index2] = round((float(item)), 2)
+       #     except ValueError:
+       #         pass
+   # csv_list = sorted(parsed_csv[1:], key=itemgetter(0))
+   # csv_list.insert(0, header)
+    #with open('Ordered_EDs.csv', 'w', newline='') as csvFile:
+     #   writer = csv.writer(csvFile, delimiter=',')
+      #  writer.writerows(csv_file)
+    return csv_file
 
 
 def load_operant_file():
@@ -47,14 +46,14 @@ def load_operant_file():
 
 def align_lists():
     behavior_file = array(load_operant_file())
-    event_detection_file = array(parse_csv())
-    a = 0.00
+    event_detection_file = parse_csv()
+    #a = 0.00
     time_codes = list(map(float, behavior_file[0]))
     behaviors = list(behavior_file[1])
-    cell_time = [a] + list(map(float, event_detection_file[1:, 0]))
-    cell_name = [a] + list(event_detection_file[1:, 1])
-    cell_value = [a] + list(event_detection_file[1:, 2])
-    df = pd.DataFrame(list(zip(cell_time, cell_name, cell_value)), columns=['Time (s)', 'Cell Name', 'Cell Value'])
+    #cell_time = [a] + list(map(float, event_detection_file[1:, 0]))
+    #cell_name = [a] + list(event_detection_file[1:, 1])
+    #cell_value = [a] + list(event_detection_file[1:, 2])
+    df = pd.read_csv(event_detection_file)
     df2 = pd.DataFrame(list(zip(time_codes, behaviors)), columns=['Time Codes', 'Behavior'])
     print(df2)
 
@@ -63,18 +62,17 @@ def align_lists():
     #   df.loc[matched_array[x], 'Time Codes'] = time_codes[x]
     #  df.loc[matched_array[x], 'Behavior'] = behaviors[x]
     # df['Behavior'].fillna(method='ffill', inplace=True)
-    df.drop_duplicates(['Time (s)'], keep='first', inplace=True)
+    #df.drop_duplicates(['Time (s)'], keep='first', inplace=True)
     print(df)
 
-    df_pivot = df.pivot(index='Time (s)', columns='Cell Name', values='Cell Value')
-    df_pivot.columns = df_pivot.columns.astype(str)
-    df_pivot.drop(columns=['0.0'], inplace=True)
-    df_pivot.insert(df_pivot.shape[1], 'Reward', NaN)
-    df_pivot.insert(df_pivot.shape[1], 'ITI', NaN)
-    df_pivot.insert(df_pivot.shape[1], 'Go', NaN)
-    df_pivot.insert(df_pivot.shape[1], 'Lever Press', NaN)
-    df_pivot.insert(df_pivot.shape[1], 'NoGo', NaN)
-    df_pivot.insert(0, 'Time (s)', df['Time (s)'].values)
+    df.columns = df.columns.astype(str)
+
+    df.insert(df.shape[1], 'Reward', NaN)
+    df.insert(df.shape[1], 'ITI', NaN)
+    df.insert(df.shape[1], 'Go', NaN)
+    df.insert(df.shape[1], 'Lever Press', NaN)
+    df.insert(df.shape[1], 'NoGo', NaN)
+    #df_pivot.insert(0, 'Time (s)', df['Time (s)'].values)
 
     # i = 1
     # for x in range(len(cell_time)):
@@ -85,9 +83,9 @@ def align_lists():
     #           i +=1
 
     with pd.option_context('display.max_rows', 200, 'display.max_columns', 50):
-        print(df_pivot)
+        print(df)
 
-    return df_pivot
+    return df
 
 
 def output_csv_file():
